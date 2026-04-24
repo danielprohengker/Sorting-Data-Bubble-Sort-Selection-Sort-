f52e354e616ee1066e402791bd2dcc7cdfe38368
danielprohengker <blsmk.250127027@budiluhur.sch.id>
2026-04-24 14:03:09 +0700

    Create kasus1.py

---
+++ kasus1.py
@@ -0,0 +1,8 @@
+# bubble sort)
+tinggi = [150, 170, 155, 160, 145]
+n = len(tinggi)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if tinggi[j] > tinggi[j+1]:
+            tinggi[j], tinggi[j+1] = tinggi[j+1], tinggi[j]
+print(f"Hasil Akhir: {tinggi}")
